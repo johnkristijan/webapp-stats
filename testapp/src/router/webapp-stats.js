7484410cd846d6737e4d51cfee2dedbb25f8c4bc
John K Hermann <john.hermann@webstep.no>
2022-10-16 23:40:10 +0200

    updated main script

--- testapp/src/router/webapp-stats.js
+++ testapp/src/router/webapp-stats.js
@@ -25,19 +25,23 @@ const sendStats = (statList) => {
   const body = JSON.stringify(statList);
   const method = "POST";
   const requestOptions = { method, headers, body };
-  fetch("http://localhost:3030/stats-eater", requestOptions);
+  fetch("http://localhost:3030/webapp-stats", requestOptions);
 };
 
 const webappStatTrack = async (to, from) => {
   try {
     ifUndefinedPopulateWithEmptyList();
+    const username = undefined
+    const baseUrl = 'http://localhost'
+    const senderDebounceLimit = 5 // controls how often you send "save data api"
     const currentList = load();
     const timestamp = new Date().toISOString();
     currentList.push({
       app_id: "746425d2-1629-46ea-a487-13738d9a99b8", // use env variable or similar
       timestamp,
-      from_path: from.path,
-      to_path: to.path,
+      user: username || 'anonymous',
+      from_path: baseUrl + from.path,
+      to_path: baseUrl + to.path,
       screen_width: window.innerWidth || "",
       screen_height: window.innerHeight || "",
     });
@@ -45,7 +49,7 @@ const webappStatTrack = async (to, from) => {
 
     const listLength = currentList.length;
 
-    if (listLength > 10) {
+    if (listLength > senderDebounceLimit) {
       sendStats(currentList);
       clearLocalStorage()
     }
